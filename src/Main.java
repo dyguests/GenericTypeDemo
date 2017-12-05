9a4bb11a46e20fa4a60d258a766cceadae069c17
fanhl <fanhonglin@klicen.com>
2017-12-05 18:36:58 +0800

    wrap

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
-public class Main {
-    public static void main(String[] args) {
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
+
+public class Main {
+
+    private static Gson gson;
+
+    public static void main(String[] args) {
+        gson = new Gson();
+        Wrap<Foo> wrap = new Wrap<>(new Foo("233"));
+        String json = gson.toJson(wrap);
+        Type type = new TypeToken<Wrap<String>>() {
+        }.getType();
+        Object obj1 = gson.fromJson(json, type);
+        System.out.println(obj1);
+        genericErase(wrap);
+    }
+
+    private static <T> void genericErase(Wrap<T> wrap) {
+        String json = gson.toJson(wrap);
+        Type type = new TypeToken<Wrap<T>>() {
+        }.getType();
+        Object obj2 = gson.fromJson(json, type);
+        System.out.println(obj2);
+    }
+}
+
+class Wrap<T> {
+    T t;
+
+    Wrap(T t) {
+        this.t = t;
+    }
+}
+
+class Foo {
+    String s;
+
+    Foo(String s) {
+        this.s = s;
     }
 }
